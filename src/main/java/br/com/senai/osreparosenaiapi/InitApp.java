64241108189b04713fc950bfb07ae51a193995dd
Alan Corrêa <alnn.correa@gmail.com>
2022-05-19 11:05:38 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/senai/osreparosenaiapi/InitApp.java
@@ -0,0 +1,13 @@
+package br.com.senai.osreparosenaiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsReparoSenaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(initApp.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/osreparosenaiapi/OsReparoSenaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.osreparosenaiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsReparoSenaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
