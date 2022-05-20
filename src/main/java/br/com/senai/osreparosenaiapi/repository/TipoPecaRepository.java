c12cdae7640c5d2c5f3181774e0c10e2ff41ebae
Alan Corrêa <alnn.correa@gmail.com>
2022-05-19 21:48:16 -0300

    Corrigido TipoPecaRepository.java Modelado classe TipoPecaService

--- src/main/java/br/com/senai/osreparosenaiapi/repository/TipoPecaRepository.java
+++ src/main/java/br/com/senai/osreparosenaiapi/repository/TipoPecaRepository.java
@@ -4,13 +4,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import br.com.senai.osreparosenaiapi.entity.Peca;
 import br.com.senai.osreparosenaiapi.entity.TipoPeca;
 
 public interface TipoPecaRepository extends JpaRepository<TipoPeca, Integer> {
 
 	@Query(value = "SELECT tp " + "FROM TipoPeca tp " + "WHERE Upper(tp.descricao) LIKE Upper(:desc)")
 
-	List<Peca> listarPor(@Param("desc") String descricao);
+	List<TipoPeca> listarPor(@Param("desc") String descricao);
 
 }
