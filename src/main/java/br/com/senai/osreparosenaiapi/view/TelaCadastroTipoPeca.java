423ce7fffacfb05e54b056d294f4c8680765f1e4
Alan Corrêa <alnn.correa@gmail.com>
2022-05-19 22:18:35 -0300

    Início da modelagem da tela de cadastro Início da modelagem da tela de pesquisa

--- src/main/java/br/com/senai/osreparosenaiapi/view/TelaCadastroTipoPeca.java
+++ src/main/java/br/com/senai/osreparosenaiapi/view/TelaCadastroTipoPeca.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class TelaCadastroTipoPeca extends JFrame {
 
@@ -31,12 +33,22 @@ public class TelaCadastroTipoPeca extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaCadastroTipoPeca() {
+		setTitle("Cadastro de Tipo de Peça");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 650, 150);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 624, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 101, Short.MAX_VALUE)
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
--- src/main/java/br/com/senai/osreparosenaiapi/view/TelaPesquisaTipoPeca.java
+++ src/main/java/br/com/senai/osreparosenaiapi/view/TelaPesquisaTipoPeca.java
@@ -31,8 +31,9 @@ public class TelaPesquisaTipoPeca extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPesquisaTipoPeca() {
+		setTitle("Pesquisa de Tipo de Peça");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 650, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
